da6b701a0dff1a1c024e330ff752e9f2c4429748
SerhiiRybak <sergey24rybak@gmail.com>
2024-05-17 17:24:34 +0300

    Initial commit

---
+++ backend/src/main/java/com/sergosoft/notes_manager/NotesManagerApplication.java
@@ -0,0 +1,13 @@
+package com.sergosoft.notes_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotesManagerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/sergosoft/notes_manager/NotesManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sergosoft.notes_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
